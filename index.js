783023dba4c80ab66a78e660274d95158503b1ae
shumaila-Rehman <160052356+shumaila-Rehman@users.noreply.github.com>
2024-03-17 16:41:40 +0500

    Add files

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter  first Number :",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter second Number :",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log(" Invalid operation");
+}
